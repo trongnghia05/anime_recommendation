b6ae0a6e9c76309b35d03293a58a92a0267ec9ca
duytanisme <duytanisme02@gmail.com>
2025-03-24 16:51:19 +0700

    change main.py to hdfs_to_es.py for better understanding and refactor io_cluster.py

--- app/src/io_cluster.py
+++ app/src/io_cluster.py
@@ -1,43 +1,56 @@
 import json
-from pyspark.sql import DataFrame
 
-def save_dataframes_to_hdfs(path,config,data_dfs,target_file_names):
+from pyspark.sql import DataFrame
+from typing import List, Dict, Any
+
+from config import Config
+
+
+def save_dataframes_to_hdfs(
+    path: str,
+    config: Config,
+    data_dfs: List[DataFrame],
+    target_file_names: List[str],
+):
     """
-        Function to store dataframe in hdfs
-        
-        Input:
-        
+    Function to store dataframe in hdfs
+    Args:
         path: the directory path to store dataframe to
         config: Config object
         data_dfs: list of PySpark DataFrames to write
-        target_file_names: list of file names to store dataframes by        
+        target_file_names: list of file names to store dataframes by
     """
 
-    for data_df,target_file_name in zip(data_dfs,target_file_names):
-        print("Processing file: ",target_file_name)
-        print("Processing dataframe of type ",type(data_df))
-        data_df.write.format("json").mode("overwrite").save(config.get_hdfs_namenode()+"/"+path+"/"+target_file_name)
+    for data_df, target_file_name in zip(data_dfs, target_file_names):
+        print("Processing file: ", target_file_name)
+        print("Processing dataframe of type ", type(data_df))
+        data_df.write.format("json").mode("overwrite").save(
+            config.get_hdfs_namenode() + "/" + path + "/" + target_file_name
+        )
 
-def save_dataframes_to_elasticsearch(dataframes,indices,es_write_config):
+
+def save_dataframes_to_elasticsearch(
+    dataframes: List[DataFrame],
+    indices: List[Any],
+    es_write_config: Dict[str, Any],
+):
     """
-       Helper function to save PySpark DataFrames to elasticsearch cluster
-       Parameters
-       ----------
-       dataframes: list of all PySpark DataFrames
-       indices: list of elasticsearch indices
-       es_write_config: dict of elasticsearch write config
+    Helper function to save PySpark DataFrames to elasticsearch cluster
+    Args:
+        dataframes: list of all PySpark DataFrames
+        indices: list of elasticsearch indices
+        es_write_config: dict of elasticsearch write config
     """
 
-    for dataframe,index in zip(dataframes,indices):
-        print("Processing index:",index)
-
-        es_write_config['es.resource'] = index
-
+    for dataframe, index in zip(dataframes, indices):
+        es_write_config["es.resource"] = index
         rdd_ = dataframe.rdd
-        rdd_.map(lambda row: (None, \
-                              json.dumps(row.asDict()))) \
-                              .saveAsNewAPIHadoopFile(path='-', \
-                              outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat", \
-                              keyClass="org.apache.hadoop.io.NullWritable", \
-                              valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable", \
-                              conf=es_write_config)
+        rdd_.map(
+            lambda row: (None, json.dumps(row.asDict()))
+        ).saveAsNewAPIHadoopFile(
+            path="-",
+            outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat",
+            keyClass="org.apache.hadoop.io.NullWritable",
+            valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable",
+            conf=es_write_config,
+        )
